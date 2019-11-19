30ebb31d7297f6f716242c3e0ff99ee941d5e2a4
martin.dulovec <martin.dulovec@accenture.com>
2019-11-19 16:03:25 +0100

    Update CustomerCard

--- modules/customer-crm-app/src/CustomerButtons/CustomerButtons.js
+++ modules/customer-crm-app/src/CustomerButtons/CustomerButtons.js
@@ -6,6 +6,7 @@ import render from "enzyme/src/render";
 import {Link} from "react-router-dom";
 import {NewForm} from "../NewForm/components";
 import {CustomerCard} from "../CustomerTable/components";
+import {Col, Row} from "reactstrap";
 
 class CustomerButtons extends Component {
 
@@ -27,26 +28,46 @@ class CustomerButtons extends Component {
   render() {
     return (
       <div>
-        <div style={{float: 'right'}}>
+        <Row>
+          <Col>
+            <Row className="align-items-left">
+          <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
           <Link to="/NewForm">
-            <Button color="success"> + Novy</Button>
+            <Button color="secondary" className="btn-pill">Karta</Button>
           </Link>
-        </div>
-        <div>
+          </Col>
+          <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
           <Link to="/NewForm">
-            <Button color="danger">Zrušit</Button>
+            <Button color="secondary" className="btn-pill">Seznam</Button>
           </Link>
-        </div>
-        <div>
+          </Col>
+          <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
           <Link to="/NewForm">
-            <Button color="danger">Zrušit</Button>
+            <Button color="primary" className="btn-pill">Odeslat email</Button>
           </Link>
-        </div>
-        <div>
+          </Col>
+          <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
           <Link to="/NewForm">
-            <Button color="danger">Zrušit</Button>
+            <Button color="primary" className="btn-pill">Odeslat SMS</Button>
           </Link>
-        </div>
+          </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Row className="align-items-right">
+          <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+          <Link to="/NewForm">
+            <Button color="danger" className="btn-pill">Odstranit</Button>
+          </Link>
+          </Col>
+          <Col col="6" sm="4" md="2" xl className="mb-3 mb-xl-0">
+          <Link to="/NewForm">
+            <Button color="success" className="btn-pill"> + Novy</Button>
+          </Link>
+          </Col>
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
